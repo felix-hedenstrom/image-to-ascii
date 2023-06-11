3dca11ee9861a7c59f2305ae2e256541326cc174
Felix Hedenström <felix.hedenstrom@gmail.com>
2023-06-11 18:00:29 +0200

    Removed Monoid and Brightness typeclass

--- src/main/scala/nu/fxh/imagetoascii/ColoredPixel.scala
+++ src/main/scala/nu/fxh/imagetoascii/ColoredPixel.scala
@@ -1,6 +1,5 @@
 package nu.fxh.imagetoascii
 
-import cats.kernel.Monoid
 import nu.fxh.imagetoascii.controlcodes.Color.ColorCombination
 import nu.fxh.imagetoascii.controlcodes.{Color, Control}
 
@@ -32,16 +31,6 @@ case class ColoredPixel(red: Int, green: Int, blue: Int) {
 }
 
 object ColoredPixel {
-
-  implicit val monoid: Monoid[ColoredPixel] = new Monoid[ColoredPixel] {
-    override def empty: ColoredPixel = ColoredPixel(0, 0, 0)
-
-    override def combine(a: ColoredPixel, b: ColoredPixel): ColoredPixel =
-      ColoredPixel(a.red + b.red, a.green + b.green, a.blue + b.blue)
-  }
-
-  implicit val brightness: Brightness[ColoredPixel] = (value: ColoredPixel, ratio: Double) =>
-    ColoredPixel((value.red * ratio).toInt, (value.green * ratio).toInt, (value.blue * ratio).toInt)
 
   def fromRgbInt(rgbInt: Int): ColoredPixel = {
     import java.awt.Color
